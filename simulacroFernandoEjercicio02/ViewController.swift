bbd0213b1cd270b54f691666dbd1d5ab09240fca
Eduardo Segovia <edu92seg@gmail.com>
2025-02-07 09:19:53 -0800

    Initial Commit

---
+++ simulacroFernandoEjercicio02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simulacroFernandoEjercicio02
+//
+//  Created by Eduardo Segovia Roman on 7/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
